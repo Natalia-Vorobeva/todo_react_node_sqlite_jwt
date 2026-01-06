0652860e60d95f0a1afdfd9c8a027d26519ca287
Natalia <vorobjeva.natalia76@gmail.com>
2026-01-06 20:51:49 +0500

    env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-	base: "/todo_react_node_sqlite_jwt/",
+	base: "/",
   plugins: [react()]
 })
